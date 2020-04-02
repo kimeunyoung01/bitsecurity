ea7251323822ec6a3d0c1599cac54e862d202ea4
kimeunyoung01 <rola70@nate.com>
2020-04-02 15:44:07 +0900

    로그아웃 처리

--- bitsecurity/src/main/java/com/example/demo/SecurityConfig.java
+++ bitsecurity/src/main/java/com/example/demo/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		//로그인 페이지를 사용자가 만들어 지정해 보자
 		http.formLogin().loginPage("/login").permitAll();
 		
-		
+		//우리가 사용자정의 loginPage를 지정하면
+		//로그아웃제공하지 않아요
+		//로그아웃을 위한 서비스명도 지정해요.
+		http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.invalidateHttpSession(true);
 		
 		http.httpBasic(); 
 		//http기본 프로토콜을 사용하겠습니다.
